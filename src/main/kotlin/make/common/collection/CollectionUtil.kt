3b1bb421587f9d9676bebd235846b32280b4ae67
MakeCat <hansexpress@outlook.com>
2025-08-10 17:23:11 +0800

    true param name; add collection mapper

--- src/main/kotlin/make/common/collection/CollectionUtil.kt
+++ src/main/kotlin/make/common/collection/CollectionUtil.kt
@@ -1,16 +1,24 @@
 package make.common.collection
 
-inline fun <K, V, T> Collection<T>.map(transformer: (T) -> Pair<K, V>): MutableMap<K, V> {
+inline fun <T, R> Collection<T>.map(transform: (T) -> R): MutableList<R> {
+    val list = ArrayList<R>(size)
+    for (element in this)
+        list += transform(element)
+
+    return list
+}
+
+inline fun <K, V, T> Collection<T>.map(transform: (T) -> Pair<K, V>): MutableMap<K, V> {
     val map = HashMap<K, V>(size)
 
     for (element in this)
-        transformer(element).apply { map[first] = second }
+        transform(element).apply { map[first] = second }
 
     return map
 }
 
-inline fun <K, V> Collection<K>.mapValues(transformer: (K) -> V) =
-    map { it to transformer(it) }
+inline fun <K, V> Collection<K>.mapValues(transform: (K) -> V) =
+    map { it to transform(it) }
 
-inline fun <K, V> Collection<V>.mapKeys(transformer: (V) -> K) =
-    map { transformer(it) to it }
+inline fun <K, V> Collection<V>.mapKeys(transform: (V) -> K) =
+    map { transform(it) to it }
