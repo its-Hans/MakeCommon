2d5698590d4d698ff0c7a5b74274a04200ae1e75
MakeCat <hansexpress@outlook.com>
2025-08-11 10:45:14 +0800

    add reverse; add map mapper

--- src/main/kotlin/make/common/collection/CollectionUtil.kt
+++ src/main/kotlin/make/common/collection/CollectionUtil.kt
@@ -8,6 +8,17 @@ inline fun <T, R> Collection<T>.map(transform: (T) -> R): MutableList<R> {
     return list
 }
 
+fun <K, V> Map<K, V>.reverse() = map { k, v -> v to k }
+
+inline fun <K, V, K0, V0> Map<K, V>.map(transform: (K, V) -> Pair<K0, V0>): MutableMap<K0, V0> {
+    val map = HashMap<K0, V0>(size)
+    for (entry in this) transform(entry.key, entry.value).apply {
+        map[first] = second
+    }
+
+    return map
+}
+
 inline fun <K, V, T> Collection<T>.map(transform: (T) -> Pair<K, V>): MutableMap<K, V> {
     val map = HashMap<K, V>(size)
 
