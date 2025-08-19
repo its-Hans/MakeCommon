bfe23bd4dd965aae16e226eb84b5fac804f5ffe4
MakeCat <hansexpress@outlook.com>
2025-08-19 17:44:50 +0800

    private

--- src/main/kotlin/make/common/result/Result.kt
+++ src/main/kotlin/make/common/result/Result.kt
@@ -7,13 +7,13 @@ sealed interface Result<T, E> {
     fun expected(message: String): T
 }
 
-class Ok<T, E>(val ok: T): Result<T, E> {
+class Ok<T, E>(private val ok: T): Result<T, E> {
     override val result get() = ok
     override fun unwrap() = ok
     override fun expected(message: String): T = ok
 }
 
-class Err<T, E>(val err: E): Result<T, E> {
+class Err<T, E>(private val err: E): Result<T, E> {
     override val result get() = err
     override fun unwrap() = throw Error("result isn't ok but unwrap called")
     override fun expected(message: String) = throw Error(message)
