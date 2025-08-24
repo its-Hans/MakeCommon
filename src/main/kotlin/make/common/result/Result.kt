496bfe8fa57fdd0706730da609bb062fc520ff19
MakeCat <hansexpress@outlook.com>
2025-08-24 19:57:29 +0800

    cause;rm match

--- src/main/kotlin/make/common/result/Result.kt
+++ src/main/kotlin/make/common/result/Result.kt
@@ -1,19 +1,26 @@
 package make.common.result
 
 sealed interface Result<out T, out E> {
+    val cause: Result<*, *>?
     val result: Any?
 
     fun unwrap(): T
     fun expected(message: String): T
 }
 
-class Ok<T, E>(private val ok: T): Result<T, E> {
+class Ok<T, E>(private val ok: T,
+               override val cause: Result<*, *>? = null
+): Result<T, E>
+{
     override val result get() = ok
     override fun unwrap() = ok
     override fun expected(message: String): T = ok
 }
 
-class Err<T, E>(private val err: E): Result<T, E> {
+class Err<T, E>(private val err: E,
+                override val cause: Result<*, *>? = null
+): Result<T, E>
+{
     override val result get() = err
     override fun unwrap() = throw Error("result isn't ok but unwrap called")
     override fun expected(message: String) = throw Error(message)
