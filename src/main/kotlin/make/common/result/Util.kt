608755343c86f67051365e0b16f504a4b6f7c4bc
MakeCat <hansexpress@outlook.com>
2025-08-27 22:02:31 +0800

    显式返回值；custom expected message；动不动

--- src/main/kotlin/make/common/result/Util.kt
+++ src/main/kotlin/make/common/result/Util.kt
@@ -2,7 +2,6 @@
 package make.common.result
 
 class UnexpectedErr(err: Err<*>): Error("unexpected error: $err")
-class ExpectedErr(message: String): Exception(message)
 
 inline fun <T, E> Result<T, E>.cat(action: Err<E>.() -> Unit) {
     if (this is Err) action(this)
@@ -14,28 +13,31 @@ inline fun <T, E> Result<T, E>.dft(action: Err<E>.() -> T): T {
     return (this as Ok).result
 }
 
-inline fun <T, E> Result<T, E>.thr(action: Err<E>.() -> Throwable) =
+inline fun <T, E> Result<T, E>.thr(action: Err<E>.() -> Throwable): T =
     dft { throw action(this) }
 
-inline fun <T, E> Result<T, E>.unwrap() =
+inline fun <T, E> Result<T, E>.unw(): T =
     thr { UnexpectedErr(this) }
 
-inline fun <T, E> Result<T, E>.expected(message: String) =
-    thr { ExpectedErr(message) }
+inline fun <T, E> Result<T, E>.exp(message: Err<E>.() -> String): T =
+    thr { Exception(message()) }
 
-inline fun <T, E> Result<T, E>.ignore(match: Err<E>.() -> Boolean) {
+inline fun <T, E> Result<T, E>.exp(message: String): T =
+    exp { message }
+
+inline fun <T, E> Result<T, E>.ign(match: Err<E>.() -> Boolean) {
     cat { if (!match(this)) throw UnexpectedErr(this) }
 }
 
 // 意味不明的周文轩代码。
-inline fun <T, E, R> Result<T, E>.mapok(transform: (T) -> R): Result<R, E> {
+inline fun <T, E, R> Result<T, E>.map(transform: (T) -> R): Result<R, E> {
     return when (this) {
         is Ok -> Ok(transform(result))
         is Err -> this as Result<R, E>
     }
 }
 
-inline fun <T, E, R> Result<T, E>.maper(transform: (E) -> R): Result<T, R> {
+inline fun <T, E, R> Result<T, E>.mae(transform: (E) -> R): Result<T, R> {
     return when (this) {
         is Err -> Err(transform(error))
         is Ok -> this as Result<T, R>
