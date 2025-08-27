29cef753865fb986cd7b27e7125ddab00a55c032
MakeCat <hansexpress@outlook.com>
2025-08-27 21:34:39 +0800

    data class; more message to panic

--- src/main/kotlin/make/common/result/Result.kt
+++ src/main/kotlin/make/common/result/Result.kt
@@ -4,13 +4,13 @@ sealed interface Result<out T, out E> {
     val cause: Result<*, *>?
 }
 
-class Ok<T> @JvmOverloads
+data class Ok<T> @JvmOverloads
 constructor(
     val result: T,
     override val cause: Result<*, *>? = null
 ): Result<T, Nothing>
 
-class Err<E> @JvmOverloads
+data class Err<E> @JvmOverloads
 constructor(
     val error: E,
     override val cause: Result<*, *>? = null
--- src/main/kotlin/make/common/result/Util.kt
+++ src/main/kotlin/make/common/result/Util.kt
@@ -1,7 +1,8 @@
 @file:Suppress("nothing_to_inline")
 package make.common.result
 
-inline fun unexpected(err: Any?) = Error("unexpected error: $err")
+class UnexpectedErr(err: Err<*>): Error("unexpected error: $err")
+class ExpectedErr(message: String): Exception(message)
 
 inline fun <T, E> Result<T, E>.cat(action: Err<E>.() -> Unit) {
     if (this is Err) action(this)
@@ -17,13 +18,13 @@ inline fun <T, E> Result<T, E>.thr(action: Err<E>.() -> Throwable) =
     dft { throw action(this) }
 
 inline fun <T, E> Result<T, E>.unwrap() =
-    thr { unexpected(error) }
+    thr { UnexpectedErr(this) }
 
 inline fun <T, E> Result<T, E>.expected(message: String) =
-    thr { Exception(message) }
+    thr { ExpectedErr(message) }
 
 inline fun <T, E> Result<T, E>.ignore(match: Err<E>.() -> Boolean) {
-    cat { if (!match(this)) throw unexpected(error) }
+    cat { if (!match(this)) throw UnexpectedErr(this) }
 }
 
 // 意味不明的周文轩代码。
